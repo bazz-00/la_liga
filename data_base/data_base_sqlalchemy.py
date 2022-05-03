ce3b1a48b891336bac13c289f9d8d7493cf86b94
bazz-00 <baziukkostia@gmail.com>
2022-05-03 11:36:47 +0300

    added classes Club and Player

--- data_base/data_base_sqlalchemy.py
+++ data_base/data_base_sqlalchemy.py
@@ -1,22 +1,26 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///la_liga.db'
 db = SQLAlchemy(app)
 
 
-class Club(db.Model):
 
+class Club(db.Model):
+    __tablename__ = 'club'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    player = relationship("Player")
+
+    def __repr__(self):
+        return f'club {self.name}'
 
 
 class Player(db.Model):
-
+    __tablename__ = 'player'
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.Integer)
     name = db.Column(db.String)
@@ -24,8 +28,11 @@ class Player(db.Model):
     height = db.Column(db.Integer)
     weight = db.Column(db.Integer)
     role = db.Column(db.String)
-    club_id = db.Column(db.Integer)
+    club_id = db.Column(db.Integer, ForeignKey('club.id'))
     club = db.Column(db.String)
+
+    def __repr__(self):
+        return f'player {self.name}'
 
 
 if __name__ == '__main__':
