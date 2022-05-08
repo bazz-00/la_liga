2008c9489aff057361a201f136d63392c0396471
bazz-00 <baziukkostia@gmail.com>
2022-05-08 17:23:01 +0300

    added table User

--- data_base/sqlal.py
+++ data_base/sqlal.py
@@ -38,12 +38,18 @@ class Player(db.Model):
     def __repr__(self):
         return f'player {self.name}'
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String)
+    password = db.Column(db.String)
 
 
 
 
-# Session = sessionmaker(bind=engine)
-# session = Session()
+#Session = sessionmaker(bind=engine)
+#session = Session()
 # c = ['Алавес', 'Атлетик Бильбао', 'Атлетико Мадрид', 'Барселона', 'Бетис', 'Севилья', 'Валенсия', 'Вильярреал',
 #       'Гранада', 'Кадис', 'Леванте', 'Мальорка', 'Осасуна', 'Райо Вальекано', 'Реал Сосьедад', 'Сельта', 'Хетафе',
 #      'Эльче', 'Эспаньол', 'Реал Мадрид']
